5abec5b1405cdfbfafc563a20d6222089a2fadff
m1nsk <punch.korolev@yandex.ru>
2018-02-15 14:54:04 +0300

    new flask

--- app/app.py
+++ app/app.py
@@ -1,20 +1,5 @@
-from flask import Flask
+from app import app
 from werkzeug.contrib.fixers import ProxyFix
-app = Flask(__name__)
-from flask import request
-import time
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    if request.method == "GET":
-        for i in range(4):
-            time.sleep(3)
-            print(i, "hello")
-        return "Hello, Wqqqorld!"
-    return "Waba-duba-da!"
-
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == "__main__":
